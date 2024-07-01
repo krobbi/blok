9271be194e025fcd27ac7421a3a1b160452e47c1
krobbi <70552146+krobbi@users.noreply.github.com>
2024-07-01 21:57:53 +0100

    Add facings for S shape

--- src/shape/blocks.rs
+++ src/shape/blocks.rs
@@ -39,9 +39,9 @@ static BLOCKS: &[Blocks; Shape::COUNT * Facing::COUNT] = &[
 
     // Shape::S
     [(1, 0), (2, 0), (0, 1), (1, 1)], // Facing::Up
-    [(1, 0), (2, 0), (0, 1), (1, 1)], // Facing::Right
-    [(1, 0), (2, 0), (0, 1), (1, 1)], // Facing::Down
-    [(1, 0), (2, 0), (0, 1), (1, 1)], // Facing::Left
+    [(1, 0), (1, 1), (2, 1), (2, 2)], // Facing::Right
+    [(1, 1), (2, 1), (0, 2), (1, 2)], // Facing::Down
+    [(0, 0), (0, 1), (1, 1), (1, 2)], // Facing::Left
 
     // Shape::T
     [(1, 0), (0, 1), (1, 1), (2, 1)], // Facing::Up
